c713e0acb6fe7453e83f3d40e379516979b0615b
Yobe Nzima <yobenzima@gmail.com>
2015-10-14 20:33:08 +0200

    Yobe - coding exercise
    Yobe Nzima - coding exercise as per request from Jacqueline da Mata from
    Strata - G Labour Solutions

--- CodingExercise/PuzzleName.cs
+++ CodingExercise/PuzzleName.cs
@@ -24,7 +24,40 @@
         private static int GetAlphabeticalValue(string name)
         {
             //TODO: remove this code and add your implementation here
-            return 0;
+            //return 0;
+             
+            /*
+             * Yobe  - 14/10/2015
+             * 
+             * Get alphabetic value of name.
+             * 
+             * 1. Declare a string with all uppercase letters of alphabet.
+             * 2. Use this to get the index of every letter in the name.
+             * NOTE: Index is 0 (zero) based so add 1 to force it start at 1 = A
+             */
+
+            int iAlphabeticalValue = 0;
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            // Get each letter of the name
+            foreach (var s in name)
+            {
+                // Convert it to uppercase before comparison
+                char letter = char.ToUpper(s);
+
+                // Must be between A and Z
+                if (letter >= 'A' || letter <= 'Z')
+                {
+                    // Get index and add 1
+                    int iIndex = alphabet.IndexOf(letter) + 1;
+
+                    // Add up the indexes
+                    iAlphabeticalValue += iIndex;
+                }
+
+            }
+
+            return iAlphabeticalValue;
         }
 
         public PuzzleName(string name, int position)
--- CodingExercise/PuzzleSolver.cs
+++ CodingExercise/PuzzleSolver.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CodingExercise
 {
@@ -41,7 +46,13 @@ namespace CodingExercise
             // in the BuildPuzzleNames(...) method.
 
             //TODO: remove this code and add your implementation here
-            PuzzleName answer = puzzleNames[0];
+
+            /*
+             * Yobe     :       14/10/2015
+             * Get the name at the position passed in
+             */
+            PuzzleName answer = puzzleNames[position];
+
 
             return answer;
         }
@@ -49,7 +60,56 @@ namespace CodingExercise
         private static string[] ParseNamesFile()
         {
             //TODO: remove this code and add your implementation here
-            return new[] {"A","B","C"};
+            //return new[] {"A","B","C"};
+
+            /*
+             * Yobe     :   14/10/2015
+             * Get names from file and return string array
+             */
+            string[] namesInFile = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\names.txt").ToString().Split(',');
+
+            // This is used to build an alphabet based name as the IEnumerable list returns has other characters in the words 
+            StringBuilder nameBuilder = new StringBuilder();
+
+            // This is the return list
+            string[] loadedNames = new string[namesInFile.Length];
+
+            // Loop through the list and get final names with only letters of the alphabet
+            for (int i = 0; i <= namesInFile.Length - 1;i++)
+            {
+                // Current name
+                string name = namesInFile[i];
+
+                // Loop through the characters in name and rmove any characters that is not a letter
+                foreach (var s in name)
+                {
+                    // Use a regular expression to to only get letters
+                    Match match = Regex.Match(s.ToString(), @"^[A-Za-z]+", RegexOptions.None);
+
+                    if (match.Success)
+                    {
+                        // Make sure it all uppercase
+                        char upper = char.ToUpper(s);
+
+                        // Make sure the letters are between A and Z
+                        if (upper >= 'A' || upper <= 'Z')
+                        {
+                            // Put the name back together again
+                            nameBuilder.Append(s);
+                        }
+                    }
+                }
+
+                // Add to list
+                loadedNames[i] = nameBuilder.ToString();
+
+                // Clear the StringBuilder
+                nameBuilder.Clear();
+            }
+
+
+            return loadedNames;
+
         }
 
         private static List<PuzzleName> BuildPuzzleNames(IEnumerable<string> names)
@@ -61,7 +121,43 @@ namespace CodingExercise
             // Position is 1 based and not zero based.
 
             //TODO: remove this code and add your implementation here
-            return names.Select(n => new PuzzleName(n, -999)).ToList();
+            //return names.Select(n => new PuzzleName(n, -999)).ToList();
+
+            /*
+             * Yobe     - 14/10/2015
+             * 
+             * Build list of puzzle objects
+             * 
+             */
+            List<PuzzleName> puzzleNames = new List<PuzzleName>();
+
+            // Convert to generic list
+            List<string> sNames = names.ToList();
+
+            // Sort the list alphabetically
+            sNames.Sort();
+
+            // Loop through the list and build puzzle objects
+            for (int i = 0; i <= sNames.Count - 1; i++)
+            {
+                // Get current name
+                string name = sNames[i];
+
+                // Get position of the name in list
+                int iPosition = sNames.IndexOf(name);
+
+                // Add 1 so Position is not zero based
+                iPosition += 1;
+
+                // New object
+                PuzzleName puzzleName = new PuzzleName(name, iPosition);
+        
+
+                puzzleNames.Add(puzzleName);
+            }
+
+
+            return puzzleNames;
         }
     }
 }
